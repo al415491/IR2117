81ea9be09e9b86095eb1846227945d5d60c6dd1b
al415491 <al415491@uji.es>
2023-01-26 12:54:25 +0100

    sum v1
    compute sum

--- Sum/sum.cpp
+++ Sum/sum.cpp
@@ -4,6 +4,11 @@ int main() {
 int num, sum=0;
 std::cout << "Enter a number: ";
 std::cin >> num;
+
+for (int i=1; i<=num; i++) {
+	sum+= i;
+}
+
 std::cout << "The sum from 1 to " << num << " is " << sum << std::endl;
 return 0;
 }
