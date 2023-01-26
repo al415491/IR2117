6ba0cd2af5d696896f22207c6f54c588b2472574
al415491 <al415491@uji.es>
2023-01-26 13:13:32 +0100

    sum v4
    check if n>=1

--- Sum/sum.cpp
+++ Sum/sum.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cassert>
 
 int sum(int num){
 	int suma = ((1+num)*num)/2 ;
@@ -10,6 +11,12 @@ int main() {
 int num, suma;
 std::cout << "Enter a number: ";
 std::cin >> num;
+
+if (num<1) {
+	std::cout << "ValueError --> Number must be equal or greater than 1\n";
+	return 0;
+	}
+
 suma = sum(num);
 std::cout << "The sum from 1 to " << num << " is " << suma << std::endl;
 return 0;
