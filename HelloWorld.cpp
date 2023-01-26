e9e5028749cbfd687d654ac11e50609e650b3539
al415491 <al415491@uji.es>
2023-01-26 12:31:52 +0100

    V1
    We ask the name with cin

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 
 int main() {
-std::cout << "Hello world!" << std::endl;
+std::string name;
+std::cout << "What is your name?" << std::endl;
+std::cin >> name;
+std::cout << "Hello " << name << "!" << std::endl;
 return 0;
 }
