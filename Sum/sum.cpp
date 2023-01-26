aa4b01ca0b512e7918c40d880d12afee66fe4812
al415491 <al415491@uji.es>
2023-01-26 12:59:38 +0100

    sum v2
    arithmetic sum

--- Sum/sum.cpp
+++ Sum/sum.cpp
@@ -5,9 +5,8 @@ int num, sum=0;
 std::cout << "Enter a number: ";
 std::cin >> num;
 
-for (int i=1; i<=num; i++) {
-	sum+= i;
-}
+// arithmetic sum
+sum = ((1+num)*num)/2 ;
 
 std::cout << "The sum from 1 to " << num << " is " << sum << std::endl;
 return 0;
