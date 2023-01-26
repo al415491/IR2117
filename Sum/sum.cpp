1a0f6b0003dedb8f0caab6af9a0009899f466da9
al415491 <al415491@uji.es>
2023-01-26 12:49:48 +0100

    sum v0
    sum = 0

---
+++ Sum/sum.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+int main() {
+int num, sum=0;
+std::cout << "Enter a number: ";
+std::cin >> num;
+std::cout << "The sum from 1 to " << num << " is " << sum << std::endl;
+return 0;
+}
