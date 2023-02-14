afa277e6510972c0ce7ef92a7a4fa17b0d8fce51
al415491 <al415491@uji.es>
2023-02-14 11:31:44 +0100

    v3

--- arith_mean/arith_mean.cpp
+++ arith_mean/arith_mean.cpp
@@ -2,7 +2,7 @@
 int main(int argc, char** argv) {
 	double m = 0, s = 0;
 	for ( int i=0, i<argc; i++) {
-		s+= argv[i];
+		s+= atoi(argv[i];) // atoi convierte en int
 	}
 	m = s/n 
 	std::cout << "Arithmetic mean: " << m << std::endl;
