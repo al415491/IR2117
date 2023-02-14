422f69b392ff3679db0037574394f017afe33901
al415491 <al415491@uji.es>
2023-02-14 11:43:48 +0100

    v4

--- arith_mean/arith_mean.cpp
+++ arith_mean/arith_mean.cpp
@@ -1,10 +1,18 @@
 #include <iostream>
-int main(int argc, char** argv) {
+int main() {
 	double m = 0, s = 0;
-	int n = argc -1;
-	for ( int i=0; i<argc; i++) {
-		s+= atoi(argv[i]); // atoi convierte en int
+	int n = 0, input;
+	
+	std::cout << "Give me a number (0 to finish): " << std::endl;
+	std::cin >> input;
+	
+	while( input != 0 ) {
+		s+= input;
+		n++;
+		std::cout << "Give me a number (0 to finish): " << std::endl;
+		std::cin >> input;
 	}
+	
 	m = s/n; 
 	std::cout << "Arithmetic mean: " << m << std::endl;
 	return 0;
