56cc39a70e6a3f581b0a7094045099915c765ab8
al415491 <al415491@uji.es>
2023-02-14 11:50:55 +0100

    v6

--- arith_mean/arith_mean.cpp
+++ arith_mean/arith_mean.cpp
@@ -6,7 +6,7 @@ int main() {
 	std::cout << "Give me a number (0 to finish): " << std::endl;
 	std::cin >> input;
 	
-	while( !std::cin.eof() ) {  // Ctrl + D para indicar end of file
+	while( not std::cin.eof() ) {  // Ctrl + D para indicar end of file
 		s+= input;
 		n++;
 		std::cout << "Give me a number (0 to finish): " << std::endl;
