c08d736d670c85fc95a0731926caa2234d1c6034
al415491 <al415491@uji.es>
2023-02-14 12:23:18 +0100

    v7

--- arith_mean/arith_mean.cpp
+++ arith_mean/arith_mean.cpp
@@ -1,18 +1,21 @@
 #include <iostream>
+#include <vector>
 int main() {
 	double m = 0, s = 0;
 	int n = 0, input;
-	
+	std::vector<int> elements;
 	std::cout << "Give me a number (0 to finish): " << std::endl;
 	std::cin >> input;
 	
 	while( not std::cin.eof() ) {  // Ctrl + D para indicar end of file
-		s+= input;
+		elements.push_back(input);
 		n++;
 		std::cout << "Give me a number (0 to finish): " << std::endl;
 		std::cin >> input;
 	}
-	
+	for ( int = 0; i< n; i++ ) {
+		s+= elements[i];	
+	}
 	m = s/n; 
 	std::cout << "Arithmetic mean: " << m << std::endl;
 	return 0;
