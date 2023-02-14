12e559ea980ef15f33d94bf2fa829a4fe137309e
al415491 <al415491@uji.es>
2023-02-14 11:30:52 +0100

    v2

--- arith_mean/arith_mean.cpp
+++ arith_mean/arith_mean.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 int main(int argc, char** argv) {
-	double m= 0;
+	double m = 0, s = 0;
+	for ( int i=0, i<argc; i++) {
+		s+= argv[i];
+	}
+	m = s/n 
 	std::cout << "Arithmetic mean: " << m << std::endl;
 	return0;
 }
