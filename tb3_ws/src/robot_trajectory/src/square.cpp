d78729ce29fa43eefecf4d500299e4b9d1303403
al415491 <al415491@uji.es>
2023-02-16 12:33:07 +0100

    move 1m and stop

--- tb3_ws/src/robot_trajectory/src/square.cpp
+++ tb3_ws/src/robot_trajectory/src/square.cpp
@@ -10,18 +10,22 @@ int main(int argc, char * argv[])
   auto node = rclcpp::Node::make_shared("publisher");
   auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
   geometry_msgs::msg::Twist message;
-  rclcpp::WallRate loop_rate(500ms);
+  rclcpp::WallRate loop_rate(10ms);
 
-  while (rclcpp::ok()) {
-  	message.linear.x = 1.0;
-  	message.angular.z = 1.0;
+  int i=0, n=1000;
+  while (rclcpp::ok() && (i<n)) {
+  	i++;
+  	message.linear.x = 0.1;
+  	message.angular.z = 0;
     publisher->publish(message);
     rclcpp::spin_some(node);
     loop_rate.sleep();
   }
+  message.linear.x = 0;
+  publisher->publish(message);
   rclcpp::shutdown();
   return 0;
 }
 
-
-
+// Voy a 0.1 m/s --> 1m en 10s --> 10s = 10 000ms
+// Mando looprates de 10ms así que utilizamos 10000/10 iteraciones = 1000
