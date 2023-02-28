45b931fbad7ba5ff22ce804191701edbbce8da6f
al415491 <al415491@uji.es>
2023-02-28 11:49:17 +0100

    summatrix v1

---
+++ Statistics/summatrix.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <Eigen/Dense>
+
+using Eigen::MatrixXd;
+
+int main() {
+    int rows, cols; 
+    std::cout << "Enter matrix size " << std::endl;
+    std::cin >> rows;
+
+    MatrixXd m(rows, cols);
+    std::cout << "Enter elements (row-wise) " << std::endl;
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++){
+            std::cin >> m(i,j);
+        }
+    }
+    std::cout << "Matrix is " << std::endl;
+    std::cout << m << std::endl;
+    return 0;
+}
