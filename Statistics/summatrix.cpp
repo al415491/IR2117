2b90b06d237bd67479c83ecb8604f64eb8a885d7
al415491 <al415491@uji.es>
2023-02-28 12:05:02 +0100

    summatrix v5

--- Statistics/summatrix.cpp
+++ Statistics/summatrix.cpp
@@ -8,6 +8,7 @@ int main() {
     int rows, cols; 
     std::cout << "Enter matrix size " << std::endl;
     std::cin >> rows;
+    cols = rows;
 
     MatrixXd m(rows, cols);
     std::cout << "Enter elements (row-wise) " << std::endl;
@@ -20,10 +21,16 @@ int main() {
     std::cout << m << std::endl;
 
     VectorXd s(rows);
+    VectorXd sc(cols);
     for (int i=0; i<rows; i++){
         s(i) = m.row(i).sum();
     }
-    std::cout << "The sum of row 0 is " << s << std::endl;
+    for (int i=0; i<cols; i++){
+        sc(i) = m.col(i).sum();
+    }
+
+    std::cout << "The sum of rows is " << s << std::endl;
+    std::cout << "The sum of columns is " << sc << std::endl;
     double maxRowSum = s.maxCoeff();
     std::cout << "The maximum sum of rows is " << maxRowSum << std::endl;
 
