b6a4e91c975259603d5aed33431bbaf4b68e0436
al415491 <al415491@uji.es>
2023-03-02 12:39:55 +0100

    square_odom v3

--- tb3_ws/src/robot_trajectory/src/square_odom.cpp
+++ tb3_ws/src/robot_trajectory/src/square_odom.cpp
@@ -5,8 +5,14 @@
 
 using namespace std::chrono_literals;
 
+double x; 
+double y; 
+
 void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg){
-	std::cout << msg << std::endl;
+	x = msg->pose.pose.position.x;
+	y = msg->pose.pose.position.y;
+	std::cout << "Pos x: " << x << std::endl;
+	std::cout << "Pos y: " << y << std::endl;
 }
 
 int main(int argc, char * argv[]) 	
@@ -15,6 +21,7 @@ int main(int argc, char * argv[])
   auto node = rclcpp::Node::make_shared("subscriber");
   auto subscription = node->create_subscription<nav_msgs::msg::Odometry>("odom", 10, topic_callback);
   rclcpp::WallRate loop_rate(10ms);
+  
 
   rclcpp::spin(node);
   rclcpp::shutdown();
