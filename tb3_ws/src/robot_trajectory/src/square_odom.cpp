6c0275aecef5660cf88000c3c87af8548632047a
al415491 <al415491@uji.es>
2023-03-02 12:27:08 +0100

    square_odom v2

--- tb3_ws/src/robot_trajectory/src/square_odom.cpp
+++ tb3_ws/src/robot_trajectory/src/square_odom.cpp
@@ -1,58 +1,22 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
-#include "geometry_msgs/msg/twist.hpp"
+#include "nav_msgs/msg/odometry.hpp"
 #include <cmath> //M_PI
 
 using namespace std::chrono_literals;
 
+void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg){
+	std::cout << msg << std::endl;
+}
+
 int main(int argc, char * argv[]) 	
 {
   rclcpp::init(argc, argv);
-  auto node = rclcpp::Node::make_shared("square");
-  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
-  node->declare_parameter("linear_speed", 0.1);
-  node->declare_parameter("angular_speed", M_PI / 20);
-  node->declare_parameter("square_length", 1.0);
-  
-  geometry_msgs::msg::Twist message;
+  auto node = rclcpp::Node::make_shared("subscriber");
+  auto subscription = node->create_subscription<nav_msgs::msg::Odometry>("odom", 10, topic_callback);
   rclcpp::WallRate loop_rate(10ms);
 
-  double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();
-  double angular_speed = node->get_parameter("angular_speed").get_parameter_value().get<double>();
-  double square_length = node->get_parameter("square_length").get_parameter_value().get<double>();
-  for(int j=0; j<4; j++)
-  { 
-  	int linear_iterations = square_length / (0.01 * linear_speed);
-  	int i=0;
-  	while (rclcpp::ok() && (i<linear_iterations)) { // move forward 1m
-  		i++;	
-  		message.linear.x = linear_speed;
-  		message.angular.z = 0;
-    	publisher->publish(message);
-    	rclcpp::spin_some(node);
-    	loop_rate.sleep();
-  	}
-  	// send zero velocity to topic
-  	message.linear.x = 0;
-  	publisher->publish(message);
-  	
-  	
-    i=0; 
-    double angular_iterations = (M_PI_2 /angular_speed)/0.01;
-    // (tiempo en girar 90ª/velocidad) / looprate
-  	while (rclcpp::ok() && (i<angular_iterations)) { // turn 90
-  		i++;	
-  		message.linear.x = 0;
-  		message.angular.z = angular_speed;
-    	publisher->publish(message);
-    	rclcpp::spin_some(node);
-    	loop_rate.sleep();
-  	}
-  	message.angular.z = 0;
-  	publisher->publish(message);
-  }
-  
-  
+  rclcpp::spin(node);
   rclcpp::shutdown();
   return 0;
 }
