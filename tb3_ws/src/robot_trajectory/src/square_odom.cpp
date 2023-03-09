043cdc045bcfe33b60ec9fd18d9955f0b79a6500
al415491 <al415491@uji.es>
2023-03-09 14:59:26 +0100

    square odom v final

--- tb3_ws/src/robot_trajectory/src/square_odom.cpp
+++ tb3_ws/src/robot_trajectory/src/square_odom.cpp
@@ -1,19 +1,24 @@
 #include <chrono>
 #include "rclcpp/rclcpp.hpp"
 #include "nav_msgs/msg/odometry.hpp"
+#include "geometry_msgs/msg/twist.hpp"
 #include <cmath> //M_PI
 
 using namespace std::chrono_literals;
 
 double xp; 
 double yp; 
-double xo; 
-double yo;
+double orientation_x; 
+double orientation_y;
+double orientation_z; 
+double orientation_w;
 double angle;
 double initial_xp;
 double initial_yp;
 double initial_angle;
 double distance;
+double siny_cosp;
+double cospy_cosp;
  
 
 void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
@@ -21,16 +26,32 @@ void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
 	if (initial_xp == 0 && initial_yp == 0 && initial_angle == 0) {
         initial_xp = msg->pose.pose.position.x;
         initial_yp = msg->pose.pose.position.y;
-        initial_angle = std::atan2(msg->pose.pose.orientation.y, msg->pose.pose.orientation.x);
+        orientation_x = msg->pose.pose.orientation.x;
+    	orientation_y = msg->pose.pose.orientation.y;
+    	orientation_z = msg->pose.pose.orientation.z;
+    	orientation_w = msg->pose.pose.orientation.w;
+        siny_cosp = 2* (orientation_w*orientation_z + orientation_x*orientation_y);
+        cospy_cosp = 1 - 2*(orientation_y*orientation_y + orientation_z*orientation_z);
+        initial_angle = atan2(siny_cosp, cospy_cosp);
     }
 	xp = msg->pose.pose.position.x;
 	yp = msg->pose.pose.position.y;
-	xo = msg->pose.pose.orientation.x;
-	yo = msg->pose.pose.orientation.y;
-	angle = std::atan2(yo, xo);
+	orientation_x = msg->pose.pose.orientation.x;
+    orientation_y = msg->pose.pose.orientation.y;
+    orientation_z = msg->pose.pose.orientation.z;
+    orientation_w = msg->pose.pose.orientation.w;
+    siny_cosp = 2* (orientation_w*orientation_z + orientation_x*orientation_y);
+    cospy_cosp = 1 - 2*(orientation_y*orientation_y + orientation_z*orientation_z);
+    angle = atan2(siny_cosp, cospy_cosp);
+    if (angle < 0) {
+    	angle += 2 * M_PI;
+    }
+    
+  
 	distance = std::sqrt(std::pow(xp - initial_xp, 2) + std::pow(yp - initial_yp, 2));
 	std::cout << "Initial x: " << initial_xp << std::endl;
 	std::cout << "Initial y: " << initial_yp << std::endl;
+	std::cout << "Initial angle: " << initial_angle << std::endl;
 	std::cout << "Pos x: " << xp << std::endl;
 	std::cout << "Pos y: " << yp << std::endl;
 	std::cout << "Angle in radians: " << angle << std::endl;
@@ -40,32 +61,80 @@ void topic_callback(const nav_msgs::msg::Odometry::SharedPtr msg)
 
 int main(int argc, char * argv[]) 	
 {
+  
   rclcpp::init(argc, argv);
+  //subscriber
   auto node = rclcpp::Node::make_shared("subscriber");
   auto subscription = node->create_subscription<nav_msgs::msg::Odometry>("odom", 10, topic_callback);
-  rclcpp::WallRate loop_rate(10ms);
   
-
-  rclcpp::spin(node);
+  // publisher
+  auto publisher = node->create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
+  geometry_msgs::msg::Twist message;
+  rclcpp::WallRate loop_rate(10ms);
+  node->declare_parameter("linear_speed", 0.2);
+  node->declare_parameter("angular_speed", 0.2);
+  node->declare_parameter("square_length", 1.0);
+  double linear_speed = node->get_parameter("linear_speed").get_parameter_value().get<double>();
+  double angular_speed = node->get_parameter("angular_speed").get_parameter_value().get<double>();
+  double square_length = node->get_parameter("square_length").get_parameter_value().get<double>();
+  
+  const nav_msgs::msg::Odometry::SharedPtr msg;
+  
+  
+  for ( int i = 1; i<=4; i++ ) {
+  
+  
+  while (rclcpp::ok() && (distance<square_length)) // move forward 
+  {
+   	message.linear.x = linear_speed;
+  	message.angular.z = 0;
+    publisher->publish(message);
+    rclcpp::spin_some(node);
+    loop_rate.sleep();
+  }
+ 
+  // send zero velocity to topic
+  
+  
+  message.linear.x = 0;
+  publisher->publish(message);
+  rclcpp::spin_some(node);
+  loop_rate.sleep();
+  initial_xp = xp;
+  initial_yp = yp;
+  loop_rate.sleep();
+  
+  
+  double target_angle = M_PI_2 + initial_angle;
+ 
+  if (target_angle > 2*M_PI) { // Por unas céntesimas puede que el robot se quede girando en bucle
+  	target_angle = 6.28
+  	;
+  }
+  std::cout << "target angle: " << target_angle << std::endl;
+  
+  
+  while (rclcpp::ok() && (angle < (target_angle) )) // turn 90 
+  {
+   std::cout << target_angle << std::endl;
+   message.linear.x = 0;
+   if ( (target_angle - angle) > 0.25 ) {
+   	message.angular.z = angular_speed;
+   } else {
+   		message.angular.z = 0.1;
+   }
+   publisher->publish(message);
+   rclcpp::spin_some(node);
+   loop_rate.sleep();
+  }
+  message.angular.z = 0;
+  publisher->publish(message);
+  initial_angle = angle;
+  loop_rate.sleep(); 
+  }
+  
   rclcpp::shutdown();
   return 0;
 }
 
 
-
-
-
-// Voy a 0.1 m/s --> 1m en 10s --> 10s = 10 000ms
-// Mando looprates de 10ms así que utilizamos 10000/10 iteraciones = 1000
-
-
-/*
-90 grados = pi/2 radianes
-Tiempo necesario para girar pi/2 radianes a 0.1 rad/s:
-t = (pi/2) / 0.1 = 5*pi segundos
-
-En un looprate de 10 ms, el número de iteraciones necesarias sería:
-
-iteraciones = (5*pi segundos) / (0.01 segundos/iteración) = 1570.8 iteraciones
-
-*/
