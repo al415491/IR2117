84ca3b95a54c1300772f3c4100ed6e19682f7e7f
al415491 <al415491@uji.es>
2023-03-28 12:05:16 +0200

    detector with publisher

--- tb3_ws/src/obstacles/src/detector.cpp
+++ tb3_ws/src/obstacles/src/detector.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include "rclcpp/rclcpp.hpp"
 #include "sensor_msgs/msg/laser_scan.hpp"
+#include "example_interfaces/msg/bool.hpp"
 
+std::shared_ptr< rclcpp::Publisher<example_interfaces::msg::Bool> > publisher;
 
-
-void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+void callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
    {
 	
 
@@ -17,8 +18,9 @@ int main(int argc, char * argv[])
 
   rclcpp::init(argc, argv);
   auto node = rclcpp::Node::make_shared("detector");
-  auto subscription = node->create_subscription<sensor_msgs::msg::LaserScan>("scan", 10, topic_callback);
-  rclcpp::spin_some(node); 
+  auto subscription = node->create_subscription<sensor_msgs::msg::LaserScan>("scan", 10, callback);
+  publisher = node->create_publisher<example_interfaces::msg::Bool>("obstacle", 10);
+  rclcpp::spin(node); 
   rclcpp::shutdown();
   return 0;
 }
