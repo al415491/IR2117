9af4b133018bd7d2ed4fef72ec7ed2ba180f572f
al415491 <al415491@uji.es>
2023-03-28 11:48:24 +0200

    detector.cpp

---
+++ tb3_ws/src/obstacles/src/detector.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/laser_scan.hpp"
+
+
+
+void topic_callback(const sensor_msgs::msg::LaserScan::SharedPtr msg)
+   {
+	
+
+
+   }
+    
+
+int main(int argc, char * argv[]) 	
+{
+
+  rclcpp::init(argc, argv);
+  auto node = rclcpp::Node::make_shared("detector");
+  auto subscription = node->create_subscription<sensor_msgs::msg::LaserScan>("scan", 10, topic_callback);
+  rclcpp::spin_some(node); 
+  rclcpp::shutdown();
+  return 0;
+}
+
+
